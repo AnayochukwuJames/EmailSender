c235f73ea40f1a5167ebcb3421f49aaf871635b3
AnayochukwuJames <anayochukwujames11@gmail.com>
2024-02-08 04:28:07 +0100

    first commit

---
+++ src/main/java/com/example/emaisenderapi/EmaiSenderApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.emaisenderapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmaiSenderApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmaiSenderApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/emaisenderapi/EmaiSenderApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.emaisenderapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmaiSenderApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
